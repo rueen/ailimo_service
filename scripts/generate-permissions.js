49848b9b6fe5e6ca1d6e96271f68c39a8c3e38cf
diaochan <diaochan@seatent.com>
2026-01-16 17:17:03 +0800

    权限

--- scripts/generate-permissions.js
+++ scripts/generate-permissions.js
@@ -35,8 +35,9 @@ const PERMISSION_CONFIG = {
       // 组织机构
       { 
         name: '组织机构',
-        code: 'organization:list',
+        code: 'organization',
         children: [
+          { name: '组织机构列表', code: 'organization:list' },
           { name: '新增组织机构', code: 'organization:create' },
           { name: '编辑组织机构', code: 'organization:update' },
           { name: '删除组织机构', code: 'organization:delete' },
@@ -45,8 +46,9 @@ const PERMISSION_CONFIG = {
       // 课题组
       { 
         name: '课题组',
-        code: 'research_group:list',
+        code: 'research_group',
         children: [
+          { name: '课题组列表', code: 'research_group:list' },
           { name: '新增课题组', code: 'research_group:create' },
           { name: '编辑课题组', code: 'research_group:update' },
           { name: '删除课题组', code: 'research_group:delete' },
@@ -71,8 +73,9 @@ const PERMISSION_CONFIG = {
       // 设备管理
       {
         name: '设备管理',
-        code: 'equipment:list',
+        code: 'equipment',
         children: [
+          { name: '设备列表', code: 'equipment:list' },
           { name: '设备详情', code: 'equipment:detail' },
           { name: '新增设备', code: 'equipment:create' },
           { name: '编辑设备', code: 'equipment:update' },
@@ -82,8 +85,9 @@ const PERMISSION_CONFIG = {
       // 时间段管理
       {
         name: '时间段管理',
-        code: 'equipment_time_slot:list',
+        code: 'equipment_time_slot',
         children: [
+          { name: '时间段列表', code: 'equipment_time_slot:list' },
           { name: '新增时间段', code: 'equipment_time_slot:create' },
           { name: '编辑时间段', code: 'equipment_time_slot:update' },
           { name: '删除时间段', code: 'equipment_time_slot:delete' },
@@ -106,8 +110,9 @@ const PERMISSION_CONFIG = {
       // 笼位管理
       {
         name: '笼位管理',
-        code: 'cage:list',
+        code: 'cage',
         children: [
+          { name: '笼位列表', code: 'cage:list' },
           { name: '新增笼位', code: 'cage:create' },
           { name: '编辑笼位', code: 'cage:update' },
           { name: '删除笼位', code: 'cage:delete' },
@@ -116,8 +121,9 @@ const PERMISSION_CONFIG = {
       // 笼位用途管理
       {
         name: '用途管理',
-        code: 'cage_purpose:list',
+        code: 'cage_purpose',
         children: [
+          { name: '用途列表', code: 'cage_purpose:list' },
           { name: '新增用途', code: 'cage_purpose:create' },
           { name: '编辑用途', code: 'cage_purpose:update' },
           { name: '删除用途', code: 'cage_purpose:delete' },
@@ -126,8 +132,9 @@ const PERMISSION_CONFIG = {
       // 时间段管理
       {
         name: '时间段管理',
-        code: 'cage_time_slot:list',
+        code: 'cage_time_slot',
         children: [
+          { name: '时间段列表', code: 'cage_time_slot:list' },
           { name: '新增时间段', code: 'cage_time_slot:create' },
           { name: '编辑时间段', code: 'cage_time_slot:update' },
           { name: '删除时间段', code: 'cage_time_slot:delete' },
@@ -150,8 +157,9 @@ const PERMISSION_CONFIG = {
       // 操作内容管理
       {
         name: '操作内容管理',
-        code: 'operation_content:list',
+        code: 'operation_content',
         children: [
+          { name: '操作内容列表', code: 'operation_content:list' },
           { name: '新增操作内容', code: 'operation_content:create' },
           { name: '编辑操作内容', code: 'operation_content:update' },
           { name: '删除操作内容', code: 'operation_content:delete' },
@@ -160,8 +168,9 @@ const PERMISSION_CONFIG = {
       // 时间段管理
       {
         name: '时间段管理',
-        code: 'experiment_time_slot:list',
+        code: 'experiment_time_slot',
         children: [
+          { name: '时间段列表', code: 'experiment_time_slot:list' },
           { name: '新增时间段', code: 'experiment_time_slot:create' },
           { name: '编辑时间段', code: 'experiment_time_slot:update' },
           { name: '删除时间段', code: 'experiment_time_slot:delete' },
@@ -184,8 +193,9 @@ const PERMISSION_CONFIG = {
       // 动物品牌管理
       {
         name: '品牌管理',
-        code: 'animal_brand:list',
+        code: 'animal_brand',
         children: [
+          { name: '品牌列表', code: 'animal_brand:list' },
           { name: '新增品牌', code: 'animal_brand:create' },
           { name: '编辑品牌', code: 'animal_brand:update' },
           { name: '删除品牌', code: 'animal_brand:delete' },
@@ -194,8 +204,9 @@ const PERMISSION_CONFIG = {
       // 动物种类/品系管理
       {
         name: '品系管理',
-        code: 'animal_variety:list',
+        code: 'animal_variety',
         children: [
+          { name: '品系列表', code: 'animal_variety:list' },
           { name: '新增品系', code: 'animal_variety:create' },
           { name: '编辑品系', code: 'animal_variety:update' },
           { name: '删除品系', code: 'animal_variety:delete' },
@@ -204,8 +215,9 @@ const PERMISSION_CONFIG = {
       // 动物规格管理
       {
         name: '规格管理',
-        code: 'animal_specification:list',
+        code: 'animal_specification',
         children: [
+          { name: '规格列表', code: 'animal_specification:list' },
           { name: '新增规格', code: 'animal_specification:create' },
           { name: '编辑规格', code: 'animal_specification:update' },
           { name: '删除规格', code: 'animal_specification:delete' },
@@ -214,8 +226,9 @@ const PERMISSION_CONFIG = {
       // 动物要求管理
       {
         name: '要求管理',
-        code: 'animal_requirement:list',
+        code: 'animal_requirement',
         children: [
+          { name: '要求列表', code: 'animal_requirement:list' },
           { name: '新增要求', code: 'animal_requirement:create' },
           { name: '编辑要求', code: 'animal_requirement:update' },
           { name: '删除要求', code: 'animal_requirement:delete' },
@@ -238,8 +251,9 @@ const PERMISSION_CONFIG = {
       // 试剂品牌管理
       {
         name: '品牌管理',
-        code: 'reagent_brand:list',
+        code: 'reagent_brand',
         children: [
+          { name: '品牌列表', code: 'reagent_brand:list' },
           { name: '新增品牌', code: 'reagent_brand:create' },
           { name: '编辑品牌', code: 'reagent_brand:update' },
           { name: '删除品牌', code: 'reagent_brand:delete' },
@@ -248,8 +262,9 @@ const PERMISSION_CONFIG = {
       // 试剂规格管理
       {
         name: '规格管理',
-        code: 'reagent_specification:list',
+        code: 'reagent_specification',
         children: [
+          { name: '规格列表', code: 'reagent_specification:list' },
           { name: '新增规格', code: 'reagent_specification:create' },
           { name: '编辑规格', code: 'reagent_specification:update' },
           { name: '删除规格', code: 'reagent_specification:delete' },
@@ -262,11 +277,17 @@ const PERMISSION_CONFIG = {
     name: '内容管理',
     code: 'content',
     children: [
-      { name: '案例管理', code: 'case:list' },
-      { name: '案例详情', code: 'case:detail' },
-      { name: '新增案例', code: 'case:create' },
-      { name: '编辑案例', code: 'case:update' },
-      { name: '删除案例', code: 'case:delete' },
+      {
+        name: '案例管理',
+        code: 'case',
+        children: [
+          { name: '案例列表', code: 'case:list' },
+          { name: '案例详情', code: 'case:detail' },
+          { name: '新增案例', code: 'case:create' },
+          { name: '编辑案例', code: 'case:update' },
+          { name: '删除案例', code: 'case:delete' },
+        ]
+      },
       { name: '公司信息', code: 'company_info' }
     ]
   },
@@ -277,8 +298,9 @@ const PERMISSION_CONFIG = {
     children: [
       {
         name: '负责人管理',
-        code: 'handler:list',
+        code: 'handler',
         children: [
+          { name: '负责人列表', code: 'handler:list' },
           { name: '新增负责人', code: 'handler:create' },
           { name: '编辑负责人', code: 'handler:update' },
           { name: '删除负责人', code: 'handler:delete' },
@@ -286,8 +308,9 @@ const PERMISSION_CONFIG = {
       },
       {
         name: '环境类型',
-        code: 'environment_type:list',
+        code: 'environment_type',
         children: [
+          { name: '环境类型列表', code: 'environment_type:list' },
           { name: '新增环境类型', code: 'environment_type:create' },
           { name: '编辑环境类型', code: 'environment_type:update' },
           { name: '删除环境类型', code: 'environment_type:delete' },
@@ -295,8 +318,9 @@ const PERMISSION_CONFIG = {
       },
       {
         name: '动物类型',
-        code: 'animal_type:list',
+        code: 'animal_type',
         children: [
+          { name: '动物类型列表', code: 'animal_type:list' },
           { name: '新增动物类型', code: 'animal_type:create' },
           { name: '编辑动物类型', code: 'animal_type:update' },
           { name: '删除动物类型', code: 'animal_type:delete' },
@@ -311,8 +335,9 @@ const PERMISSION_CONFIG = {
     children: [
       {
         name: '管理员管理',
-        code: 'administrator:list',
+        code: 'administrator',
         children: [
+          { name: '管理员列表', code: 'administrator:list' },
           { name: '新增管理员', code: 'administrator:create' },
           { name: '编辑管理员', code: 'administrator:update' },
           { name: '删除管理员', code: 'administrator:delete' },
@@ -320,8 +345,9 @@ const PERMISSION_CONFIG = {
       },
       {
         name: '角色管理',
-        code: 'role:list',
+        code: 'role',
         children: [
+          { name: '角色列表', code: 'role:list' },
           { name: '新增角色', code: 'role:create' },
           { name: '编辑角色', code: 'role:update' },
           { name: '删除角色', code: 'role:delete' },
